ea44805bc9a5093636f5c1039394349889f992db
talisong-cardoso <talisong.cardoso@gmail.com>
2022-05-21 12:49:51 -0300

    login button and show password button configured

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,48 @@
 let sliderDisplay = document.querySelector(".slider img");
 
-let i = 1;
+let i = 0;
 
 setInterval(() => {
-    (i > 4) ? i = 1 : i++;
+    (i >= 4) ? i = 1 : i++;
     sliderDisplay.src = `./img/slider_${i}.png`;
-    console.log(sliderDisplay);
 }, 5000);
+
+const userNameInput = document.querySelector("#user_name");
+const passwordInput = document.querySelector("#password");
+const loginButton = document.querySelector("#login_button");
+const passwordShowButton = document.querySelector("#password_show_button");
+
+passwordShowButton.addEventListener("click", showPassword);
+
+window.addEventListener("keyup", () => {
+    if (userNameInput.value.length > 0 && passwordInput.value.length > 5) {
+        enableLoginButton();
+    } else {
+        loginButton.disabled = true;
+    }
+    displayShowPasswordButton();
+});
+
+function enableLoginButton() {
+    loginButton.disabled = false;
+}
+
+function showPassword() {
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        passwordShowButton.innerHTML = "Ocultar";
+    } else {
+        passwordInput.type = "password";
+        passwordShowButton.innerHTML = "Mostrar";
+    }
+}
+
+function displayShowPasswordButton() {
+    if (passwordInput.value.length < 1) {
+        passwordShowButton.style.display = "none";
+        passwordInput.type = "password";
+    } else {
+        passwordShowButton.style.display = "block";
+        passwordShowButton.innerHTML = "Mostrar";
+    }
+};
